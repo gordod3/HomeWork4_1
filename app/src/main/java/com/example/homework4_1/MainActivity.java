ce790b3aac93b9bb07e0c37fc693d01ff76a82aa
User <dr.gordod>
2020-05-06 20:25:12 +0300

    V.1.9.

--- app/src/main/java/com/example/homework4_1/MainActivity.java
+++ app/src/main/java/com/example/homework4_1/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        homeFragment = (HomeFragment) manager.findFragmentById(R.id.nav_host_fragment);
     }
 
     @Override
